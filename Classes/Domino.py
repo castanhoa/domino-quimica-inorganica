ea28b16d8436fbc0ef6b18adbfc00ba35f827580
infinitybr08 <60942296+infinitybr08@users.noreply.github.com>
2026-04-13 11:34:06 -0300

    Create Domino.py

---
+++ Classes/Domino.py
@@ -0,0 +1,13 @@
+import random
+class Peca:
+  valor = []
+  local = 0
+# 0 = monte, 1 = mão do jogador 1, 2 = mão da IA
+  dupla = False
+  def forma(valor):
+    if valor[0] == valor [1]:
+      dupla == True
+    for n in len(valor):  
+      match valor[n]:
+        'acido': 
+          random.choise(listaDeAcidos.)
