3de74c391cc290a2256fe7b221f158b709c04c31
infinitybr08 <60942296+infinitybr08@users.noreply.github.com>
2026-04-15 11:43:47 -0300

    Enhance Aluno class with game statistics methods
    Added methods to track game statistics and calculate win rate.

--- Classes/Aluno.py
+++ Classes/Aluno.py
@@ -5,7 +5,14 @@ class Aluno(Usuario):
         super().__init__(senha, nome, logado_ao_criar_conta)
         self.__id_turma = id_turma
         self.__taxa_vitoria = 1
-
+        self.__partidas_jogadas = 0
+        self.__dificuldade = 0
+        self.__partidas_vencidas = 0
     # esqueleto apenas. nada pronto.
-    def ver_estatisticas():
-        pass
+    def ver_estatisticas(self):
+        self.__taxa_vitoria = self.__partidas_jogadas/self.__partidas_vencidas
+        return self.__taxa_vitoria
+    def resultado(self, victory:bool):
+        self.__partidas_jogadas +=1
+        if victory:
+            self.__partidas_vencidas +=1
