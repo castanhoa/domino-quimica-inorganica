edaae6925cd17d382f8353c77b13148dd0298a6d
Caio Mantovani Garrido <gmcaio2020@gmail.com>
2026-04-15 09:40:30 -0300

    Atualizar Domino.py
    Concerto de bugs

--- Classes/Domino.py
+++ Classes/Domino.py
@@ -4,28 +4,26 @@ class Peca:
     self.valor = [valor1, valor2]
     self.local = 0
 # 0 = monte, 1 = mão do jogador 1, 2 = mão da IA
-    if valor[0] == valor[1]:
+    if valor1 == valor2:
       self.dupla =  True
     else:
       self.dupla = False
   def forma(self):
-    if self.valor[0] == self.valor [1]:
-      dupla == True
-    for n in len(valor):
-      chance = randint(1, 100)
+    for n in range(2):
+      chance = random.randint(1, 100)
       #escolhe como a peça será mostrada,ainda falta atribuir às peças diretamente aos arquivos de imagem
       #Formulas aleatórias
-      if chance >= probabilidadeFormula
+      if chance <= probabilidadeFormula
         match self.valor[n]:
           case 'acido':
-            random.choice(formulasAcidos.)
+            random.choice(formulasAcidos)
           case 'basico':
             random.choice(formulasBases)
           case 'sal':
             random.choice(formulasSais)
           case 'oxido':
             random.choice(formulasOxidos)
-      elif chance >= probabilidadeFormula + probabilidadeConceito:
+      elif chance <= probabilidadeFormula + probabilidadeConceito:
       #falta adicionar o código pega a parte da peça com conceito
         match self.valor[n]:
           case 'acido':
