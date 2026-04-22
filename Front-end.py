7b9a6c6e30c202fa88b95dcb2f4c9c639b340d5b
joao-hcn <26.01448-0@maua.br>
2026-04-22 12:55:11 -0300

    python

---
+++ Front-end.py
@@ -0,0 +1,22 @@
+
+import pygame
+import pygame_gui
+# Inicialização
+pygame.init()
+# Configuração da tela
+largura, altura = 800, 600
+tela = pygame.display.set_mode((largura, altura))
+pygame.display.set_caption("Primeiro front end")
+# Loop principal
+rodando = True
+while rodando:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            rodando = False
+
+# Cor de fundo (Preto)
+tela.fill((0, 0, 0))
+# --- Desenhe seus botões/textos aqui ---
+# Atualiza a tela
+pygame.display.flip()
+pygame.quit()
