99a8863af93f6b8521fb00c1bc14e1ec2c9968ba
joao-hcn <26.01448-0@maua.br>
2026-05-13 13:10:48 -0300

    Add responsive layout handling to Tela class
    Agora é possível alterar o tamanho da tela

--- Front-end/Tela.py
+++ Front-end/Tela.py
@@ -2,8 +2,14 @@ class classeTela:
     def __init__(self, largura=800, altura=600, titulo=""):
         import pygame
         pygame.init()
+
         self.pygame = pygame
-        self.tela = pygame.display.set_mode((largura, altura))
+
+        self.largura_base = largura
+        self.altura_base = altura
+
+        self.tela = pygame.display.set_mode((largura, altura), pygame.RESIZABLE)
+
         self.proxima_tela = None
 
         pygame.display.set_caption(titulo)
@@ -11,13 +17,41 @@ class classeTela:
         self.clock = pygame.time.Clock()
         self.rodando = True
 
+        self.elementos_responsivos = []
+
+    def registrar_rect(self, nome, x, y, largura, altura):
+        self.elementos_responsivos.append({"nome": nome, "x": x, "y": y, "largura": largura, "altura": altura})
+
+        setattr(self, nome, self.pygame.Rect(x, y, largura, altura))
+
+    def atualizar_layout(self):
+        largura_atual = self.tela.get_width()
+        altura_atual = self.tela.get_height()
+
+        escala_x = largura_atual / self.largura_base
+        escala_y = altura_atual / self.altura_base
+
+        for item in self.elementos_responsivos:
+            novo_rect = self.pygame.Rect(
+                int(item["x"] * escala_x),
+                int(item["y"] * escala_y),
+                int(item["largura"] * escala_x),
+                int(item["altura"] * escala_y))
+
+            setattr(self, item["nome"], novo_rect)
+
     def executar(self):
         while self.rodando:
             self.clock.tick(60)
 
             for evento in self.pygame.event.get():
+
                 if evento.type == self.pygame.QUIT:
                     self.rodando = False
+
+                if evento.type == self.pygame.VIDEORESIZE:
+                    self.tela = self.pygame.display.set_mode((evento.w, evento.h), self.pygame.RESIZABLE)
+                    self.atualizar_layout()
 
                 self.tratar_eventos(evento)
 
@@ -27,7 +61,7 @@ class classeTela:
             if self.proxima_tela:
                 self.transicao()
                 return self.proxima_tela
-        
+
         self.pygame.quit()
 
     def transicao(self):
@@ -40,7 +74,7 @@ class classeTela:
             self.tela.blit(fade, (0, 0))
             self.pygame.display.flip()
             self.pygame.time.delay(20)
-    
+
     def tratar_eventos(self, evento):
         pass
 
