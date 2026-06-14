fffcde7ed3768d95efb4cc21896350f719ef1023
joao-hcn <26.01448-0@maua.br>
2026-06-14 10:45:35 -0300

    Update TelaRelatorio.py

--- Conteudo/TelaRelatorio.py
+++ Conteudo/TelaRelatorio.py
@@ -16,7 +16,7 @@ class TelaRelatorio(classeTela):
         self.velocidade_animacao = 0.1
         
         self.fonte = pygame.font.SysFont("roboto", tamanho_fonte(20))
-        self.logo = pygame.image.load(r"C:\Users\maria\Desktop\Logotipo.png")
+        self.logo = pygame.image.load(r"C:\Users\26-01448-0\Desktop\Logotipo.png")
 
         pygame.display.set_icon(self.logo)
     
