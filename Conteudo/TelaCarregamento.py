9e64d066d878c23262013c070e1eae2688a525f1
joao-hcn <26.01448-0@maua.br>
2026-06-14 10:49:47 -0300

    Integrate accessibility color adjustments in loading screen
    Refactor loading screen to use accessibility color adjustment and add a loading function.

--- Conteudo/TelaCarregamento.py
+++ Conteudo/TelaCarregamento.py
@@ -1,3 +1,4 @@
+from Acessibilidade import ajustar_cor
 from Tela import classeTela
 import pygame
 import math
@@ -12,16 +13,12 @@ class TelaCarregamento(classeTela):
         self.angulo = 0
 
     def desenhar_pontos(self, centro_x, centro_y, raio=30):
-
         pontos = 8  # quantidade de pontos no círculo
 
         for i in range(pontos):
-
             angulo = self.angulo + (2 * math.pi * i / pontos)
-
             x = centro_x + math.cos(angulo) * raio
             y = centro_y + math.sin(angulo) * raio
-
             intensidade = int(255 * (i / pontos))
             cor = (intensidade, intensidade, intensidade)
 
@@ -29,9 +26,7 @@ class TelaCarregamento(classeTela):
 
 
     def executar(self):
-
         while self.rodando:
-
             self.clock.tick(60)
 
             for event in pygame.event.get():
@@ -39,30 +34,20 @@ class TelaCarregamento(classeTela):
                     self.rodando = False
 
             # fundo
-            self.tela.fill((255, 255, 255))
-
+            self.tela.fill(ajustar_cor(255, 255, 255))
             # calcula faixa superior
             tamanho_faixa = int(self.altura * 0.15)
             y_faixa = 0
-
             # desenha faixa vermelha
-            pygame.draw.rect(
-                self.tela,
-                (255, 0, 0),
-                (0, y_faixa, self.largura, tamanho_faixa)
-            )
-
+            pygame.draw.rect(self.tela, ajustar_cor(255, 0, 0), (0, y_faixa, self.largura, tamanho_faixa))
             # texto central
             texto = self.fonte.render("Carregando...", True, (0, 0, 0))
             texto_rect = texto.get_rect(center=(self.largura // 2, self.altura // 2 - 80))
             self.tela.blit(texto, texto_rect)
-
             # animação dos pontos
             self.desenhar_pontos(self.largura // 2, self.altura // 2)
-
             # rotação
             self.angulo += 0.1
-
             # tempo de execução
             self.tempo += self.clock.get_time() / 1000
 
@@ -70,3 +55,7 @@ class TelaCarregamento(classeTela):
 
             if self.tempo >= self.duracao:
                 self.rodando = False
+
+    def mostrar_loading():
+        loading = TelaCarregamento(duracao = 0.8)
+        loading.executar()
